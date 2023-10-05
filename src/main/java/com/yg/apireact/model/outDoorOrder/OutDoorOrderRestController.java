b4dbf123e4716b2ed49ec40643024e31a0f64cb0
“YVGunko“ <yurygunko@gmail.com>
2023-10-05 20:53:10 +0300

    fixed bug

--- src/main/java/com/yg/apireact/model/outDoorOrder/OutDoorOrderRestController.java
+++ src/main/java/com/yg/apireact/model/outDoorOrder/OutDoorOrderRestController.java
@@ -2,16 +2,12 @@ package com.yg.apireact.model.outDoorOrder;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.validation.constraints.NotNull;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -30,37 +26,36 @@ import com.yg.apireact.utils.Utils;
 @RestController
 //we allow cors requests from our frontend environment
 //note the curly braces that creates an array of strings ... required by the annotation
-@CrossOrigin(
-		origins =  {"http://localhost:8082", "http://localhost:3000"}, 
-		methods = {RequestMethod.GET, 
-				RequestMethod.OPTIONS,
-				RequestMethod.POST, 
-				RequestMethod.PUT,
-				RequestMethod.DELETE,
-				RequestMethod.PATCH,
-				RequestMethod.HEAD})
+@CrossOrigin(origins = { "http://localhost:8082", "http://localhost:3000" }, methods = { RequestMethod.GET,
+		RequestMethod.OPTIONS, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.PATCH,
+		RequestMethod.HEAD })
 @RequestMapping("/api/orders")
 public class OutDoorOrderRestController {
 	private static final int DETAILS_LENGTH = 25;
-	
-	@Autowired OutDoorOrderRepository repo;
-	@Autowired OutDoorOrderService service;
-	@Autowired OutDoorOrderRowService rowService;
-	@Autowired UserRepository userRepo;
+
+	@Autowired
+	OutDoorOrderRepository repo;
+	@Autowired
+	OutDoorOrderService service;
+	@Autowired
+	OutDoorOrderRowService rowService;
+	@Autowired
+	UserRepository userRepo;
 
 	@CrossOrigin(origins = { "http://localhost:8082", "http://localhost:3000" }, methods = { RequestMethod.GET,
 			RequestMethod.OPTIONS })
 	@RequestMapping(value = "", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<List<OutDoorOrderReq>> getOrders(
-			@RequestParam(name = "userId", required = false) Long userId,
-			@RequestParam(name = "dateFrom", required = false) Date dateFrom,
-			@RequestParam(name = "dateTill", required = false) Date dateTill) {
+	public ResponseEntity<List<OutDoorOrderReq>> getOrders(@RequestParam(name = "userId", required = false) Long userId,
+			@RequestParam(name = "dateFrom", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") Date dateFrom,
+			@RequestParam(name = "dateTill", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") Date dateTill) {
 
 		try {
 			List<OutDoorOrderReq> responce = new ArrayList<>();
 			List<OutDoorOrder> pageTuts = new ArrayList<>();
-			if (dateFrom == null) dateFrom = Utils.toDate(Utils.startOfMonth());
-			if (dateTill == null) dateTill = Utils.toDate(Utils.endOfMonth());
+			if (dateFrom == null)
+				dateFrom = Utils.toDate(Utils.startOfMonth());
+			if (dateTill == null)
+				dateTill = Utils.toDate(Utils.endOfMonth());
 			if (userId != null) {
 				userRepo.findById(userId).orElseThrow(() -> new IllegalArgumentException(
 						"userId not found exception. userId=".concat(userId.toString())));
@@ -72,8 +67,9 @@ public class OutDoorOrderRestController {
 			for (OutDoorOrder b : pageTuts) {
 				// String id, String sDate, String sClient, String sDivision, String sDesc,
 				// String sUser
-				responce.add(new OutDoorOrderReq(b.getId(), b.getComment(), rowService.getGoods(b.getId(), DETAILS_LENGTH),
-						b.getCustomer().getId(), b.getCustomer().getName(), b.getDivision().getCode(), b.getDivision().getName(),
+				responce.add(new OutDoorOrderReq(b.getId(), b.getComment(),
+						rowService.getGoods(b.getId(), DETAILS_LENGTH), b.getCustomer().getId(),
+						b.getCustomer().getName(), b.getDivision().getCode(), b.getDivision().getName(),
 						String.valueOf(b.getUser().getId()), b.getUser().getName(), b.getSample(), b.getDate()));
 			}
 
@@ -82,47 +78,47 @@ public class OutDoorOrderRestController {
 			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
-	
-	@CrossOrigin(origins =  {"http://localhost:8082", "http://localhost:3000"}, methods = {RequestMethod.GET, RequestMethod.OPTIONS})
+
+	@CrossOrigin(origins = { "http://localhost:8082", "http://localhost:3000" }, methods = { RequestMethod.GET,
+			RequestMethod.OPTIONS })
 	@RequestMapping(value = "{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<OutDoorOrderReq> getOrderById(@PathVariable @NotNull String id) {
-		//TODO
+		// TODO
 		try {
 			OutDoorOrder b = repo.findById(id).orElseThrow();
-			OutDoorOrderReq response =  new OutDoorOrderReq(b.getId(), b.getComment(), rowService.getGoods(b.getId(), DETAILS_LENGTH),
-	   				b.getCustomer().getId(), b.getCustomer().getName(), b.getDivision().getCode(), b.getDivision().getName(),  
-	 				String.valueOf(b.getUser().getId()), b.getUser().getName(), 
-	 				b.getSample(), b.getDate());
+			OutDoorOrderReq response = new OutDoorOrderReq(b.getId(), b.getComment(),
+					rowService.getGoods(b.getId(), DETAILS_LENGTH), b.getCustomer().getId(), b.getCustomer().getName(),
+					b.getDivision().getCode(), b.getDivision().getName(), String.valueOf(b.getUser().getId()),
+					b.getUser().getName(), b.getSample(), b.getDate());
 			return new ResponseEntity<>(response, HttpStatus.OK);
-	    } catch (Exception e) {
-	      return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-	    }
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 
-	@CrossOrigin(origins =  {"http://localhost:8082", "http://localhost:3000"}, methods = {RequestMethod.POST})
-	@RequestMapping(value = "", method = {RequestMethod.POST}, produces = MediaType.APPLICATION_JSON_VALUE)
+	@CrossOrigin(origins = { "http://localhost:8082", "http://localhost:3000" }, methods = { RequestMethod.POST })
+	@RequestMapping(value = "", method = { RequestMethod.POST }, produces = MediaType.APPLICATION_JSON_VALUE)
 
-	public ResponseEntity<OutDoorOrderReq> post (@RequestBody OutDoorOrderReq request) {
+	public ResponseEntity<OutDoorOrderReq> post(@RequestBody OutDoorOrderReq request) {
 		try {
 			OutDoorOrderReq response = service.saveOrUpdate(request);
 			return new ResponseEntity<>(response, HttpStatus.OK);
-	    } catch (Exception e) {
-	      return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-	    }
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
-	
-	@CrossOrigin(origins =  {"http://localhost:8082", "http://localhost:3000"}, methods = {
-			RequestMethod.PUT, 
-			RequestMethod.PATCH})
-	@RequestMapping(value = "{id}", method = {RequestMethod.PUT, 
+
+	@CrossOrigin(origins = { "http://localhost:8082", "http://localhost:3000" }, methods = { RequestMethod.PUT,
+			RequestMethod.PATCH })
+	@RequestMapping(value = "{id}", method = { RequestMethod.PUT,
 			RequestMethod.PATCH }, produces = MediaType.APPLICATION_JSON_VALUE)
 
 	public ResponseEntity<OutDoorOrderReq> putPatch(@RequestBody OutDoorOrderReq request) {
 		try {
 			OutDoorOrderReq response = service.saveOrUpdate(request);
 			return new ResponseEntity<>(response, HttpStatus.OK);
-	    } catch (Exception e) {
-	      return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-	    }
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 }
